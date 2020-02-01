14ba408b61c22acfb8e5d1f90c7daffc712a3cdd
Peter Witzel <peter@p-zentrale.de>
2020-02-01 13:24:52 +0100

    min width/height constrains, store window pos

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-const { app, BrowserWindow } = require('electron')
+const electron = require("electron");
+const { app, BrowserWindow } = electron;
+const settings = require("electron-settings");
+
+const minWidth = 950;
+const minHeight = 720;
 
 let win
 
 function createWindow () {
-  
+  const rect = storedWindowPos();
   win = new BrowserWindow({
-    width: 950,
-    height: 720,
+    x: rect?rect.x:undefined,
+    y: rect?rect.y:undefined,
+    width: rect?rect.width:minWidth,
+    height: rect?rect.height:minHeight,
+    minWidth: minWidth,
+    minHeight: minHeight,
     autoHideMenuBar: true,
     icon: __dirname + '/res/zoner.png',
     webPreferences: {
@@ -15,6 +24,10 @@ function createWindow () {
   })
   win.loadFile('index.html')
   // win.webContents.openDevTools()
+
+  win.on('close', ()=> {
+    saveWindowPos(win);
+  });
 
   win.on('closed', () => {
     win = null
@@ -26,3 +39,39 @@ app.on('ready', createWindow)
 app.on('window-all-closed', () => {
     app.quit()  
 })
+
+function saveWindowPos(win) {
+  if (!win.isMinimized()) {
+    let rect = win.getContentBounds();
+    settings.set('windowPos', rect);
+  }
+}
+
+function storedWindowPos() {
+  let rect = settings.get('windowPos');
+  if (rect) {
+    // check if stored window pos is within screen
+    if (rect.width < minWidth) rect.width = minWidth;
+    if (rect.height < minHeight) rect.height = minHeight;
+    let displays = electron.screen.getAllDisplays();
+    let isVisible = false;
+    const max_thresh = 100;
+    displays.forEach(display => {
+      let bounds = display.bounds;
+      if (
+        rect.x >= bounds.x &&
+        rect.x < bounds.x + bounds.width - max_thresh &&
+        rect.y >= bounds.y &&
+        rect.y < bounds.y + bounds.height - max_thresh
+      ) {
+        isVisible = true;
+      }
+    });
+    if (isVisible) {
+      return rect;
+    } else {
+      return undefined;
+    }
+  }
+  return rect;
+}
