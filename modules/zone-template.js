bafb8ccd19d61943749de77a9afe01824d2f57b0
Peter Witzel <peter@privatepublic.de>
2024-04-03 16:51:04 +0200

    cc controller element template

--- modules/zone-template.js
+++ modules/zone-template.js
@@ -2,20 +2,28 @@ const Zone = require('./zone');
 const fs = require('fs');
 const path = require('path');
 
-const template = fs.readFileSync(
+const templateZone = fs.readFileSync(
   path.join(__dirname, '../res/zone.template.html'),
+  'utf8'
+);
+
+const templateController = fs.readFileSync(
+  path.join(__dirname, '../res/controller.template.html'),
   'utf8'
 );
 
 function stripTooltips(/** @type {string} */ s) {
   return s.replace(/title=\".+?\"/g, '');
 }
-function interpolateTemplate(template, args) {
-  return Object.entries(args).reduce(
-    (result, [arg, val]) => result.replaceAll(`$\{${arg}}`, `${val}`),
-    template
+
+const interpolateTemplate = function (templateString, templateVars) {
+  const func = new Function(
+    ...Object.keys(templateVars),
+    'return `' + templateString + '`;'
   );
-}
+  return func(...Object.values(templateVars));
+};
+
 let cycleConditions = '<optgroup>';
 let lastCycleCond = 2;
 for (let cond of Zone.Sequence.CYCLE_CONDITIONS) {
@@ -67,71 +75,11 @@ module.exports = {
   getControllerHTML: function (/** @type {Zone} */ zone, zoneindex) {
     let controllers = '';
     zone.cc_controllers.forEach((cc, ix) => {
-      controllers += /*html*/ `
-        <div class="ccpot ${
-          cc.type == 2 ? 'spacer' : ''
-        }" id="pot_${zoneindex}_${ix}">
-            <div class="ccpot-inner">
-              <div class="ccpot-front">
-                <svg
-                  width="100%"
-                  height="100%"
-                  viewBox="0 0 56 56"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    id="pot_range_${zoneindex}_${ix}"
-                    d=""
-                    stroke="var(--darker-1)"
-                    stroke-width="10"
-                    fill="none"
-                  />
-                  <path
-                    id="pot_value_${zoneindex}_${ix}"
-                    d=""
-                    stroke="var(--brighter-4)"
-                    stroke-width="6"
-                    fill="none"
-                  />
-                  <rect
-                    id="pot_zero_${zoneindex}_${ix}"
-                    x="27" y="8" width="2" height="8" fill="var(--brighter-2)" />
-                </svg>
-                <div class="cclabel">
-                  ${cc.label}
-                </div>
-                <span class="value">127</span>
-              </div>
-              <div class="ccpot-back">
-                <div><input class="cclabel" type="text" value="${
-                  cc.label
-                }" data-change="${zoneindex}:cc_label:${ix}"/>
-                </div>
-                <div class="cc">
-                  <input type="text" class="cc-in" 
-                    value="${cc.number_in || cc.number}" 
-                    data-change="${zoneindex}:cc_number_in:${ix}" 
-                    data-focus-change="${zoneindex}:cc_focused:${ix}"
-                    title="Enter input cc number.\nAny CC input from MIDI-In controller is learned if this control is selected!"
-                   />
-                  <span>&rarr;</span>
-                  <input type="text" class="cc-out"
-                    value="${cc.number}" 
-                    data-change="${zoneindex}:cc_number:${ix}"
-                    title="Output cc number"
-                  />
-                </div>
-                <div class="cc_change_type" data-action="${zoneindex}:cc_change_type:${ix}">mode</div>
-                <div class="cc_tools">
-                  <span class="material-icons" title="Move left" data-action="${zoneindex}:cc_left:${ix}">arrow_back</span>
-                  <span class="material-icons" title="Remove" data-action="${zoneindex}:cc_remove:${ix}">close</span>
-                  <span class="material-icons" title="Add new" data-action="${zoneindex}:cc_add:${ix}">add</span>
-                  <span class="material-icons" title="Move right" data-action="${zoneindex}:cc_right:${ix}">arrow_forward</span>
-                </div>
-              </div>
-            </div>
-          </div>
-      `;
+      controllers += interpolateTemplate(templateController, {
+        cc: cc,
+        ix: ix,
+        zoneindex: zoneindex
+      });
     });
     return controllers; //stripTooltips(controllers);
   },
@@ -151,7 +99,7 @@ module.exports = {
     }
     const zoneMuteKeyboardHint =
       index < 10 ? `('${(index + 1) % 10}' on computer keyboard)` : '';
-    return interpolateTemplate(template, {
+    return interpolateTemplate(templateZone, {
       index: index,
       zoneMuteKeyboardHint: zoneMuteKeyboardHint,
       channelselector: channelselector,
