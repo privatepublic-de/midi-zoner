5ab8104987191c0ea1258917d097d83ad2c2ad23
Peter Witzel <peter@privatepublic.de>
2024-12-25 13:28:27 +0100

    display kb shortcut seq on/off

--- modules/zone-template.js
+++ modules/zone-template.js
@@ -88,7 +88,9 @@ module.exports = {
       index < 10 ? `('${(index + 1) % 10}' on computer keyboard)` : '';
     return interpolateTemplate(templateZone, {
       index: index,
-      zoneNumber: index + 1,
+      zoneNumber: `${index + 1} ${
+        index < 10 ? 'QWERTZUIOP'.charAt(index) : '-'
+      }`,
       zoneMuteKeyboardHint: zoneMuteKeyboardHint,
       channelselector: channelselector,
       sequencerGrid: sequencerGrid,
