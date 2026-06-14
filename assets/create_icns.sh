093de47545fb7741d1eb14791eb4de80838153c6
Peter Witzel <peter@privatepublic.de>
2026-06-14 16:08:44 +0200

    updated icons, removed migration docs

--- assets/create_icns.sh
+++ assets/create_icns.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
+set -e
 
-    file="${1}"
-    iconset="$(mktemp -d)"
-    output_icon="$(mktemp).icns"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+file="${1:-"${SCRIPT_DIR}/zoner.png"}"
+iconset="$(mktemp -d)"
+output_icon="$(mktemp).icns"
 
-    for size in {16,32,64,128,256,512}; do
-    sips --resampleHeightWidth "${size}" "${size}" "${file}" --out "${iconset}/icon_${size}x${size}.png" &> /dev/null
-    sips --resampleHeightWidth "$((size * 2))" "$((size * 2))" "${file}" --out "${iconset}/icon_${size}x${size}@2x.png" &> /dev/null
-    done
+for size in 16 32 64 128 256 512; do
+    sips --resampleHeightWidth "${size}" "${size}" "${file}" --out "${iconset}/icon_${size}x${size}.png"
+    sips --resampleHeightWidth "$((size * 2))" "$((size * 2))" "${file}" --out "${iconset}/icon_${size}x${size}@2x.png"
+done
 
-    mv "${iconset}" "${iconset}.iconset"
-    iconutil --convert icns "${iconset}.iconset" --output "${output_icon}"
+mv "${iconset}" "${iconset}.iconset"
+iconutil --convert icns "${iconset}.iconset" --output "${output_icon}"
 
-    mv "${output_icon}" ./zoner.icns
-
-
+mv "${output_icon}" "${SCRIPT_DIR}/zoner.icns"
+echo "Created ${SCRIPT_DIR}/zoner.icns"
